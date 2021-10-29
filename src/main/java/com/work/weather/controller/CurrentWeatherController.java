ac51ca985769a627013ebbd82695a07302e1eb22
rgabhawala <rgabhawala@users.noreply.github.com>
2021-10-29 16:33:47 -0700

    updating ui for weather

--- src/main/java/com/work/weather/controller/CurrentWeatherController.java
+++ src/main/java/com/work/weather/controller/CurrentWeatherController.java
@@ -1,9 +1,6 @@
 package com.work.weather.controller;
 
-import com.work.weather.model.CurrentAstronomy;
-import com.work.weather.model.CurrentExtremeTide;
-import com.work.weather.model.CurrentSolar;
-import com.work.weather.model.CurrentWeather;
+import com.work.weather.model.*;
 import com.work.weather.service.OpenWeatherService;
 import com.work.weather.service.StormGlassService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Map;
 
@@ -24,8 +22,17 @@ public class CurrentWeatherController {
     @Autowired
     private StormGlassService stormGlassService;
 
-    @RequestMapping(value = "/current-weather", method = RequestMethod.GET)
-    public ModelAndView getCurrentWeather(ModelAndView modelAndView, @RequestParam("city") String city, @RequestParam("country") String country) {
+    @RequestMapping(value = "/currentweather", method = RequestMethod.GET)
+    public ModelAndView displayCurrentWeather(ModelAndView modelAndView, WeatherLocation location) {
+        modelAndView.addObject("location", location);
+        modelAndView.setViewName("city-search");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/currentweather", method = RequestMethod.POST)
+    public ModelAndView currentWeather(ModelAndView modelAndView, WeatherLocation location) {
+        String city = location.getCity();
+        String country = location.getCountryCode();
         CurrentWeather currentWeather = openWeatherService.getCurrentWeather(city, country);
         Map<String, String> coordinates =  openWeatherService.getLatLong(city, country);
         CurrentAstronomy currentAstronomy = stormGlassService.getAstronomy(coordinates.get("lat"), coordinates.get("long"));
---
+++ src/main/java/com/work/weather/model/WeatherLocation.java
@@ -0,0 +1,23 @@
+package com.work.weather.model;
+
+public class WeatherLocation {
+
+    String city;
+    String countryCode;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+}
