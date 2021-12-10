43264777a1aff9c4dc5c55181478c4d3fff4ef27
rgabhawala <rgabhawala@users.noreply.github.com>
2021-12-09 22:09:22 -0800

    reorg

--- src/main/java/com/work/weather/controller/CurrentWeatherController.java
+++ src/main/java/com/work/weather/controller/CurrentWeatherController.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Map;
 
